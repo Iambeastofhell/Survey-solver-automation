5939efa142a768d7d2e1f2bf85377fb2c868b889
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2025-02-01 01:50:43 +0530

    added key handling in bot

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from openai import OpenAI
 from dotenv import load_dotenv
 
+from selenium.webdriver.common.keys import Keys
+
 load_dotenv()
 
 
@@ -21,6 +23,7 @@ client = OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
+KEYMAP = {k:v for k,v in Keys.__dict__.items() if not k.startswith("__") and k != 'ZENKAKU_HANKAKU'}
 
 class Bot:
     def __init__(self, prompt: str) -> None:
@@ -28,7 +31,7 @@ class Bot:
         self.history = [
             {
                 "role": "system",
-                "content": f"You are a helpful assistant. Your job is to help the user with '{prompt}'. The user will give you the current HTML of the webpage and you will respond with an action to carry out to achieve the goal of the user. After that the user will again give the HTML of the webpage and the loop continues till the task is completed, at which case return none.",
+                "content": f"You are a helpful assistant. Your job is to help the user with '{prompt}'. The user will give you the current HTML of the webpage and you will respond with an action to carry out to achieve the goal of the user. After that the user will again give the HTML of the webpage and the loop continues till the task is completed, at which case return none. For keys that are not printable characters, use this keymap for reference and follow it strictly: {KEYMAP}",
             },
         ]
 
@@ -53,3 +56,4 @@ class Bot:
         )
         print("AI decision: ", response.choices[0].message.parsed)
         return response.choices[0].message.parsed
+
