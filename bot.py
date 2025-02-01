cea16e5067624576828a2994ec08d7eeeec3281c
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2025-02-02 03:40:45 +0530

    changes

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 
+from typing import Annotated, Union
 from pydantic import BaseModel
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -18,12 +19,22 @@ class KeyBoardAction(BaseModel):
     key_to_press: str
 
 
+class Action(BaseModel):
+    action_type: str
+    action: Union[ClickAction, KeyBoardAction]
+
+
 client = OpenAI(
     api_key=os.getenv("API_KEY"),
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
-KEYMAP = {k:v for k,v in Keys.__dict__.items() if not k.startswith("__") and k != 'ZENKAKU_HANKAKU'}
+KEYMAP = {
+    k: v
+    for k, v in Keys.__dict__.items()
+    if not k.startswith("__") and k != "ZENKAKU_HANKAKU"
+}
+
 
 class Bot:
     def __init__(self, prompt: str) -> None:
@@ -35,25 +46,33 @@ class Bot:
             },
         ]
 
-    def get_action(self, html: str) -> ClickAction | KeyBoardAction | None:
+    def get_action(self, html: str) -> Action | None:
         self.history.append(
             {
                 "role": "user",
-                "content": f"The current HTML of the webpage is\n\n{html}",
+                "content": f"The current HTML of the webpage is\n\n{html}\n\nWhen outputting the query selector ensure that it is present in the html corpus supplied to you. That element will be interacted with on the webpage\n\nRespond with: " + """
+{
+'action_type': 'click' | 'keypress',
+'action': {
+    'query_selector': '',
+    'key_to_press': ''
+} | {
+    'query_selector': '', // this is for clicking
+}
+}""",
             }
         )
-        response = client.beta.chat.completions.parse(
+        response = client.chat.completions.create(
             model="gemini-1.5-flash",
             n=1,
-            messages=self.history, #type: ignore
-            response_format=ClickAction | KeyBoardAction,  # type: ignore
+            messages=self.history,  # type: ignore
+            response_format={"type": "json_object"},  # type: ignore
         )
         self.history.append(
             {
                 "role": "assistant",
-                "content": f"Action: {response.choices[0].message.parsed}",
+                "content": f"Action: {response.choices[0].message.content}",
             }
         )
-        print("AI decision: ", response.choices[0].message.parsed)
-        return response.choices[0].message.parsed
-
+        print("AI decision: ", response.choices[0].message.content)
+        return Action.model_validate_json(response.choices[0].message.content or "{}")
